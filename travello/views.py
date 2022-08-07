b39831863eda660add866bb952f206bbeb6b13eb
programmercave0 <mailabhi593@gmail.com>
2022-08-07 20:31:56 +0530

    models class created

--- travello/views.py
+++ travello/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def homepage(request):
-    return render(request,'index.html',{'price':700})
+    est=models.Destination()
+    dest.id=1
+    dest.name='Hyderabad'
+    dest.desc='The Ethnic City'
+    dest.img='destination_1.jpg'
+    dest.price=500
+    return render(request,'index.html',{'dest1':dest})
 
 def view_profile(request):
     pass
