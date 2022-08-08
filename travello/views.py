739ea8175432bf1530a3574f597753dda2d56f53
programmercave0 <mailabhi593@gmail.com>
2022-08-08 16:03:32 +0530

    calling all objects

--- travello/views.py
+++ travello/views.py
@@ -2,13 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def homepage(request):
-    dest=models.Destination()
-    dest.id=1
-    dest.name='Hyderabad'
-    dest.desc='The Ethnic City'
-    dest.img='destination_1.jpg'
-    dest.price=500
-    return render(request,'index.html',{'dest1':dest})
+    dests=Destination.objects.all()
+    return render(request,'index.html',{'dests':dests})
 
 def view_profile(request):
     pass
